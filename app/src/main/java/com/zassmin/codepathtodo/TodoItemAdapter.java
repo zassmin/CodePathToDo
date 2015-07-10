d04f684344ace542934ba8536c380da05cbe3183
zassmin <zmontesd@gmail.com>
2015-07-09 19:22:33 -0700

    implement caching with ViewHolder

--- app/src/main/java/com/zassmin/codepathtodo/TodoItemAdapter.java
+++ app/src/main/java/com/zassmin/codepathtodo/TodoItemAdapter.java
@@ -15,6 +15,10 @@ import java.util.ArrayList;
  * Created by zassmin on 7/6/15.
  */
 public class TodoItemAdapter extends ArrayAdapter<TodoItem> {
+    public static class ViewHolder {
+        TextView itemName;
+    }
+
     public TodoItemAdapter(Context context, ArrayList<TodoItem> todoItems) {
         super(context, 0, todoItems);
     }
@@ -23,13 +27,20 @@ public class TodoItemAdapter extends ArrayAdapter<TodoItem> {
     public View getView(int position, View convertView, ViewGroup parent) {
         TodoItem todoItem = getItem(position);
 
+        // Check if the existing view is being reused, otherwise, inflate the view
+        ViewHolder viewHolder;
         if (convertView == null) {
+            viewHolder = new ViewHolder();
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.todo_item, parent, false);
+            viewHolder.itemName = (TextView) convertView.findViewById(R.id.tvItemName);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        TextView tvItemName = (TextView) convertView.findViewById(R.id.tvItemName);
-        tvItemName.setText(todoItem.getItemName());
-
+        // populate the data to the template using the data object
+        viewHolder.itemName.setText(todoItem.getItemName());
+        // return the completed view to render on screen
         return convertView;
     }
 }
